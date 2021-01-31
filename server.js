bdee9308eccfb13edaeaa52bcedd4e7578282de1
Evison <evisoncy@gmail.com>
2021-01-31 13:03:42 -0500

    sajdkaslñas

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/salutem-web-official'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/salutem-web-official/index.html'));});
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/salutem-web-official/index.html'));
+});
 app.listen(process.env.PORT || 8080);
